8b8b6525e2f14feba04d2691e0666cce39ae5744
MommaWatasu <ascendwatson@gmail.com>
2022-02-15 22:36:29 +0900

    [test]fix test for Clustering
    solved the problem that learning didn't coverge during testing

--- test/Clustering.jl
+++ test/Clustering.jl
@@ -1,35 +1,13 @@
+using DataFrames
+using CSV
 using HorseML.Clustering
 using HorseML.Clustering: fit!
 
 @testset "Clustering" begin
-    #Generate data
-    N = 100
+    #Load the data
+    df = CSV.read("clustering.csv", DataFrame)
     K = 3
-    t = zeros(N, 3)
-    x = zeros(N, 2)
-    μ = [
-        -0.5 -0.5
-        0.5 1.0
-        1.0 -0.5
-    ]
-    σ = [
-        0.7 0.7
-        0.8 0.3
-        0.3 0.8
-    ]
-    _π = [0.4, 0.8, 1]
-    for n in 1 : N
-        wk = rand()
-        for k in 1 : K
-            if wk < _π[k]
-                t[n, k] = 1
-                break
-            end
-        end
-        for k in 1 : 2
-            x[n, k] = randn() * σ[findfirst(t[n, :].==1), k] + μ[findfirst(t[n, :].==1), k]
-        end
-    end
+    x = Array(df)
     
     #Test for Kmeans
     model = Kmeans(K)
